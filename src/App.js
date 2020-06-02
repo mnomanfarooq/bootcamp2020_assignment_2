97d502e094a07b73618fc172d1e4bb841654489c
Muhammad Noman Farooq <nomanfarooq@gmail.com>
2020-06-02 22:56:14 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
           by Muhammad Noman Farooq.
         </p>
         <a
